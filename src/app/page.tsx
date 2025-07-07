39fea9e83eddf70068352fc3f15579e321a7d6c5
Vedanth Bandodkar <vedanthbandodkar01@gmail.com>
2025-07-07 17:32:03 +0000

    new job posted is not showing in the my posting section of the dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { Search } from 'lucide-react';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   const jobs = await getJobs();
 
